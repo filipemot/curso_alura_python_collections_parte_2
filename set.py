1effca97290954f25d27fbe62847059e709cea18
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-05-10 15:22:14 -0300

    Operações

--- set.py
+++ set.py
@@ -4,3 +4,11 @@ usuarios_machine_learning = {13, 23, 56, 42}
 print(f"Números Sem repetição: {usuarios_data_science | usuarios_machine_learning}")
 print(f"Números repetidos: {usuarios_data_science & usuarios_machine_learning}")
 print(f"Quem está em datascience e não está em machine Learning: {usuarios_data_science - usuarios_machine_learning}")
+
+#Set Imutável
+usuarios_data_science = frozenset(usuarios_data_science)
+#usuarios_data_science.add(15)
+
+#Retirando Duplicidade de Palavras
+minha_palavra = "Meu nome é Filipe, o seu nome é Carlos"
+print(set(minha_palavra.split()))#{'o', 'nome', 'é', 'seu', 'Meu', 'Filipe,', 'Carlos'}
